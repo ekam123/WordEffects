ecb77217493597ab830ab72ca02aca7c560b4761
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-05-27 12:43:53 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Ekam Singh Dhaliwal on 2019-05-27.
+//  Copyright © 2019 ekam-singh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
